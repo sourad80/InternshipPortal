c41d80754644be066392eb07e666b6ba96aec30d
Souradeep Guha Neogi <39113709+sourad80@users.noreply.github.com>
2021-05-23 23:18:23 +0530

    Revert "ORGANIZATION"

--- model.py
+++ model.py
@@ -25,18 +25,3 @@ class Student(db.Model,UserMixin):
 
     def __repr__(self):
         return f"Student('{self.username}', '{self.email}')"
-
-@login_manager.user_loader
-def load_organization(user_id):
-    return Organization.query.get(int(user_id))
-
-class Organization(db.Model,UserMixin):
-    id = db.Column(db.Integer,primary_key=True)
-    orgname = db.Column(db.String(20), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    phone = db.Column(db.Integer, unique=True, nullable=False)
-    address = db.Column(db.String(120))
-    password = db.Column(db.String(60), nullable=False)
-
-    def __repr__(self):
-        return f"Organization('{self.orgname}', '{self.email}')"
